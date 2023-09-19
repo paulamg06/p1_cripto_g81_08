2a042e3416b52f87108c516f06c4a821c6f20c2e
paulamg06 <100472240@alumnos.uc3m.es>
2023-09-19 18:26:14 +0200

    Meto comprobaciones de fecha

--- aplicacion.py
+++ aplicacion.py
@@ -10,43 +10,59 @@ class Aplicacion(ttk.Frame):
     def __init__(self, parent):
         super().__init__(parent)
 
+        self.scroll = ttk.Scrollbar(parent, orient="vertical")
+        self.scroll.place(x=560, heigh=500, width=50)
+
         #Lista de eventos
         self.eventos = []
 
         'Texto'
-        self.bienvenida = ttk.Label(parent, text="Tus cuenta atrás: ")
+        self.bienvenida = ttk.Label(parent, text="Tus eventos: ")
         self.bienvenida.place(x=20, y=5)
 
+        self.info_evento = ttk.Label(parent, text="nombre, dd/mm/aaaa, hh:mm")
+        self.info_evento.place(x=20, y=35)
+
+        self.separador = ttk.Label(parent, text="-----------------------------------------------------")
+        self.separador.place(x=0, y=55)
+
         self.texto_nombre = ttk.Label(parent, text="Nombre del evento: ")
-        self.texto_nombre.place(x=20, y=35)
+        self.texto_nombre.place(x=20, y=75)
 
-        self.texto_fecha = ttk.Label(parent, text="Fecha del evento: ")
-        self.texto_fecha.place(x=20, y=70)
+        self.texto_fecha = ttk.Label(parent, text="Fecha del evento (dd/mm/aaaa): ")
+        self.texto_fecha.place(x=20, y=110)
 
-        self.texto_hora = ttk.Label(parent, text="Hora del evento (opcional): ")
-        self.texto_hora.place(x=20, y=105)
+        self.texto_hora = ttk.Label(parent, text="*Hora del evento (hh:mm): ")
+        self.texto_hora.place(x=20, y=145)
+
 
         'Cuadros'
         self.cuadro_nombre = ttk.Entry(parent)
-        self.cuadro_nombre.place(x=170, y=35)
+        self.cuadro_nombre.place(x=130, y=75)
 
         self.cuadro_fecha = ttk.Entry(parent)
-        self.cuadro_fecha.place(x=170, y=70)
+        self.cuadro_fecha.place(x=200, y=110, width=70)
 
         self.cuadro_hora = ttk.Entry(parent)
-        self.cuadro_hora.place(x=170, y=105)
+        self.cuadro_hora.place(x=170, y=145, width=50)
 
         'Botones'
-        self.introducir = ttk.Button(parent, text="Nuevo evento")
-        self.introducir.place(x=20, y=140)
+        self.introducir = ttk.Button(parent, text="Nuevo evento", command=self.crear_nuevo_evento)
+        self.introducir.place(x=20, y=180)
 
     def crear_nuevo_evento(self):
         """Función que crea una nueva instancia evento"""
-        nombre = self.cuadro_nombre.get()
-        fecha = self.cuadro_fecha.get()
-        hora = self.cuadro_hora.get()
+        nombre = str(self.cuadro_nombre.get())
+        fecha = str(self.cuadro_fecha.get())
+        hora = str(self.cuadro_hora.get())
+
+        #Si no se introduce ningún nombre ni fecha, no hace nada
+        if not nombre or not fecha:
+            return None
 
         if not hora:
             self.eventos.append(Evento(nombre, fecha, "9:00"))
         else:
             self.eventos.append(Evento(nombre, fecha, hora))
+
+        self.info_evento.config(text=f"Evento: {self.eventos[0].nombre}, {self.eventos[0].fecha}, {self.eventos[0].hora}")
--- evento.py
+++ evento.py
@@ -2,7 +2,43 @@
 class Evento():
     """Clase que almacena los eventos de los usuarios"""
     def __init__(self, nombre, fecha, hora):
-        self.nombre = nombre
-        self.fecha = fecha
+        self.__nombre = nombre
+        self.__fecha = fecha
         #Hay que modificar la hora para que sea opcional :)
-        self.hora = hora
+        self.__hora = hora
+
+    @property
+    def nombre(self):
+        return self.__nombre
+
+    @property
+    def fecha(self):
+        """Verificamos que está en el formato correcto (dd/mm/aaaa)"""
+        if len(self.__fecha) != 10:
+            raise Exception("El formato de la fecha es dd/mm/aaaa")
+
+        #Verificamos el año
+        year = int(self.__fecha[6:])
+        print(year)
+        if year < 2023:
+            raise Exception("Ese año ya ha pasado")
+
+        month = int(self.__fecha[3:5])
+        print(month)
+        if month < 1 or month > 12:
+            raise Exception("Introduce un mes válido")
+
+        #Verificamos el dia
+        dia = int(self.__fecha[:2])
+        print(dia)
+        if dia < 1 or dia > 31:
+            raise Exception("Introduce un dia válido")
+
+
+        return self.__fecha
+
+    @property
+    def hora(self):
+        """Verificamos que está en el formato correcto (hh:mm)"""
+
+        return self.__hora
