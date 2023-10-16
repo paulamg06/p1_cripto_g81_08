8c1d2db9b7f45c87e81396053e80045c90a9d965
100472087 <100472087@alumnos.uc3m.es>
2023-10-16 09:42:00 +0200

    agregar usuario

--- db/gestion_usuarios.py
+++ db/gestion_usuarios.py
@@ -1,31 +1,45 @@
 """Fichero que contiene el registro de los usuarios"""
+import base64
 import sqlite3 as sql
 from tkinter import messagebox
 from crypto.tokens import Tokens
 
+
 class GestionUsuarios:
     """Clase que contiene el registro de los usuarios"""
+
     def __init__(self, db_file):
         self.connection = sql.connect(db_file)
         self.cursor = self.connection.cursor()
-        self.usuarios = [] #Lista que almacena los usuarios que se registran
+        self.usuarios = []  # Lista que almacena los usuarios que se registran
 
         self.cursor.execute('''
+                REMOVE TABLE usuarios 
+                ''')
+
+        """self.cursor.execute('''
         CREATE TABLE IF NOT EXISTS usuarios 
         (id INTEGER PRIMARY KEY AUTOINCREMENT,
         usuario TEXT NOT NULL,
         token TEXT NOT NULL,
         salt TEXT NOT NULL)
-        ''')
+        ''')"""
 
         self.connection.commit()
 
     def agregar_usuario(self, usuario, contrasena):
         """Método que regitra a los usuarios añadiéndolos a la lista de usuarios"""
         try:
-            #primero derivamos la contraseña
+            # primero derivamos la contraseña
             token = Tokens(contrasena)
-            self.cursor.execute("INSERT INTO usuarios (usuario,token,salt) VALUES (?,?,?)", (usuario,token.key,token.salt))
+            key_b64 = base64.b64encode(token.key)
+            key_ascii = key_b64.decode()
+
+            salt_b64 = base64.b64encode(token.salt)
+            salt_ascii = salt_b64.decode()
+
+            self.cursor.execute("INSERT INTO usuarios (usuario,token,salt) VALUES (?,?,?)",
+                                (usuario, key_ascii, salt_ascii))
             self.connection.commit()
             return True
         except sql.Error as exception:
@@ -34,10 +48,10 @@ class GestionUsuarios:
 
     def verificar_credenciales(self, usuario, contrasena):
         """Método que verifica el inicio de sesión del usuario"""
-        #derivamos la contraseña
+        # derivamos la contraseña
         token = Tokens(contrasena)
-        #verificamos el token
-        self.cursor.execute("SELECT * FROM usuarios WHERE usuario=? AND token=?", (usuario,token.key))
+        # verificamos el token
+        self.cursor.execute("SELECT * FROM usuarios WHERE usuario=? AND token=?", (usuario, token.key))
         resultado = self.cursor.fetchone()
         return resultado is not None
 
